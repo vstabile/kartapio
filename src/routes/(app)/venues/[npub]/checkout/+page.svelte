6720e0f61d4bf7863442c1b60f3458d5d7666f62
João Thallis <joaothallis@icloud.com>
2025-02-21 13:19:14 -0300

    feat: integrate Lightning payment using @getalby/lightning-tools

--- src/routes/(app)/venues/[npub]/checkout/+page.svelte
+++ src/routes/(app)/venues/[npub]/checkout/+page.svelte
@@ -1,19 +1,30 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    import { browser } from '$app/environment';
-  
+    import { LightningAddress } from '@getalby/lightning-tools';
+
+    let launchPaymentModal: any;
+
     onMount(async () => {
-      if (browser) {
-        const { launchModal } = await import('https://esm.sh/@getalby/bitcoin-connect@3.6.3');
-        launchModal();
-      }
+        const alby = await import('@getalby/bitcoin-connect');
+
+        launchPaymentModal = alby.launchPaymentModal;
     });
-  </script>
-  
-  <svelte:head>
-    <title>Bitcoin Connect</title>
-  </svelte:head>
-  
-  <main>
-    <bc-pay-button invoice="lnbc1320n1pnm0ksadqqnp4qvgjk7awmrcflvs3582pshnd07vp5z9wmkjuqgc8lapww333cf8lupp5m5z5qgxuq4nlm3e2phwz34c68z2ugj067236w3lxdsmkyr29z93qsp5rvna4sm79xsc5pwkhsr5g3eemuwv67njs25wfr0du03hm0ys00vq9qyysgqcqpcxqyz5vqrzjqw9fu4j39mycmg440ztkraa03u5qhtuc5zfgydsv6ml38qd4azymlapyqqqqqqq2c5qqqqlgqqqq86qqjqfktmwupnymn9sltxl5jlfr5ldhhy0ahpuq3wehyjzwwnc8uuzutyflzw6jrevh59mn022332nmgweuyzy3lnqh5a4gvvf7mljxyrkpcp8gt6ec"></bc-pay-button>
-  </main>
+    async function startPayment() {
+        const amount = 21;
+
+        const address = import.meta.env.VITE_LIGHTNING_ADDRESS;
+        const ln = new LightningAddress(address);
+        await ln.fetch();
+        const invoice = await ln.requestInvoice({ satoshi: amount });
+
+        if (launchPaymentModal) {
+            launchPaymentModal({
+                invoice: invoice.paymentRequest,
+                onPaid: ({ preimage }: { preimage: string }) => alert('Received payment! ' + preimage),
+                onCancelled: () => alert('Payment cancelled')
+            });
+        }
+    }
+</script>
+
+<button on:click={startPayment}>Pay with Lightning ⚡</button>
